74c94bb1636a10e73b04e7793f2def93417e6ea0
kswisher <kenny.swisher@gmail.com>
2018-02-18 14:16:13 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
+// configue middleware to serve publicPath
+
+// app.listen
